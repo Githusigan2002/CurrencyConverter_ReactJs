bf54330ff78cc1703d144b0810f7323b66bd65a3
Githusigan2002 <jeyarupangithusigan@gmail.com>
2025-06-29 21:41:23 +0530

    Implemeted app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,48 @@
-import { useState } from 'react'
-
+import { useEffect, useState } from 'react'
+import axios from 'axios';
 import './App.css'
 
 function App() {
 
+  const [amount, setAmount] = useState(1);
+  const [fromCurrency, setFromCurrency] = useState("USD");
+  const [toCurrency, setToCurrency] = useState("LKR");
+  const [convertedAmt, setConvertedAmt] = useState(null);
+
+  useEffect(() => {
+    const getExchangeRate = async () => {
+      try {
+        let URL = `https://api.exchangerate-api.com/v4/latest/${fromCurrency}`;
+        const res = await axios.get(URL);
+        console.log(res);
+        
+
+      } catch (error) {
+        console.error(error)
+      }
+    };
+  });
+
+
+  const handleAMTchange = (e)=>{
+    const val = parseFloat(e.target.val);
+    setAmount(isNaN(val)?0:val);
+  };
   return (
     <>
       <div className='cur-con'>
         <div className="box"></div>
         <div className="data">
           <h1>Currency Converter</h1>
+
           <div className="input-container">
-            <label htmlFor="amt">Amount:</label>
-            <input type="number" id="amt" />
+            <label htmlFor="amt" >Amount:</label>
+            <input type="number" value={amount} id="amt" onChange={handleAMTchange}/>
           </div>
+
           <div className="input-container">
             <label htmlFor="amt">From Currency:</label>
-            <select name="" id="fromCurrency">
+            <select name="" id="fromCurrency" value={fromCurrency}>
               <option value="USD">USD</option>
               <option value="GBP">GBP</option>
               <option value="LKR">LKR</option>
@@ -25,9 +51,10 @@ function App() {
               <option value="JPY">JPY</option>
             </select>
           </div>
+
           <div className="input-container">
             <label htmlFor="amt">To Currency:</label>
-            <select name="" id="toCurrency">
+            <select name="" id="toCurrency" value={toCurrency}>
               <option value="USD">USD</option>
               <option value="GBP">GBP</option>
               <option value="LKR">LKR</option>
@@ -37,7 +64,7 @@ function App() {
             </select>
           </div>
           <div className="results">
-            <p>1 USD = 300 LKR</p>
+            <p>{amount} {fromCurrency} = {convertedAmt} {toCurrency}</p>
           </div>
         </div>
       </div>
